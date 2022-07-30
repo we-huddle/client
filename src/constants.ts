483772df298671352116ee1cdbf40626cc5e0fef
Gravewalker666 <heshanandrews99@gmail.com>
2022-07-30 22:09:54 +0530

    Remove test command from workflow

--- src/constants.ts
+++ src/constants.ts
@@ -5,8 +5,6 @@ export type APIConfig = {
 }
 
 export const API: APIConfig = {
-  BASE: 'http://lionfish-app-m7suk.ondigitalocean.app/',
-  // Uncomment to work on local set up, make sure this is set to the correct base url before merging PRs to master
-  // BASE: 'http://localhost:8080/',
+  BASE: process.env.NODE_ENV === "production"? 'http://lionfish-app-m7suk.ondigitalocean.app/' : 'http://localhost:8080/',
   TOKEN: undefined,
 };
