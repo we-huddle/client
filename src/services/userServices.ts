22940990243e8fe36dd75632e65da9d57b34dd8b
Gravewalker666 <heshanandrews99@gmail.com>
2022-10-25 13:01:24 +0530

    Make follow user UIs and wire them

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { API } from "../constants";
-import { Profile, PartialProfile } from "../types/Profile";
+import {API} from "../constants";
+import {Profile, PartialProfile} from "../types/Profile";
 
 export class UserServices {
   static async fetchSelf(): Promise<Profile> {
@@ -11,6 +11,43 @@ export class UserServices {
         },
       });
       return response.data;
+    } catch (e) {
+      throw new Error();
+    }
+  }
+
+  static async getFollowerList(profileId: string): Promise<Profile[]> {
+    try {
+      const response = await axios.get<Profile[]>(`${API.BASE}/profile/${profileId}/followers`);
+      return response.data;
+    } catch (e) {
+      throw new Error();
+    }
+  }
+
+  static async followUser(profileId: string) {
+    try {
+      await axios.post<void>(`${API.BASE}/user/${profileId}/follow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${API.TOKEN}`,
+          },
+        });
+    } catch (e) {
+      throw new Error();
+    }
+  }
+
+  static async unfollowUser(profileId: string) {
+    try {
+      await axios.post<void>(`${API.BASE}/user/${profileId}/unfollow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${API.TOKEN}`,
+          },
+        });
     } catch (e) {
       throw new Error();
     }
