f57f8ec0557ccf02ddf47398c43583d0f183054b
YaiyaSam <kalsha.sam26@gmail.com>
2022-10-26 01:43:05 +0530

    Edit the landing page description

--- src/scenes/PublicHome/index.tsx
+++ src/scenes/PublicHome/index.tsx
@@ -4,8 +4,9 @@ function PublicHome() {
       <div className="w-1/3 h-full font-bold mt-30 ml-20">
         <p className="text-blue-900 text-7xl mt-32">WeHuddle</p>
         <p className="font-normal text-gray-500 text-lg mt-5">
-          lorem ipsum ah jlsao hdsal hdaskd lajds al dsk daskd askdjsad sadlsajd
-          sajdlsajd hkdsa daksjd kda sdk sadkjs djsakd akjsdkas jd
+          A Github integration tool for open source organizations where it
+          allows them to manage their project sprints and keep track of their
+          contributors' performance.
         </p>
       </div>
       <div className="w-2/3 h-full -mr-10">
