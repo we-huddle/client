55f0826388c22828eb372fb05a944815e7b02088
YaiyaSam <kalsha.sam26@gmail.com>
2022-10-27 16:51:34 +0530

    Replace the loading message with a spinner

--- src/components/LoginRedirect/index.tsx
+++ src/components/LoginRedirect/index.tsx
@@ -2,6 +2,7 @@ import {useContext, useEffect} from "react";
 import {useSearchParams} from "react-router-dom";
 import UserContext from "../../types/UserContext";
 import {TOKEN_KEY} from "../../constants";
+import { Spinner } from "flowbite-react";
 
 
 function LoginRedirect() {
@@ -22,7 +23,9 @@ function LoginRedirect() {
 
 
   return (
-    <div>Loading...</div>
+    // <div>Loading...</div>
+    <div className="w-full place-items-center"><Spinner /></div>
+    
   );
 }
 
